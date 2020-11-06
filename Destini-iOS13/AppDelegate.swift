1c8664f77b8c037eb13e85cb31954c8c28f304e3
Alex Johnson <ajohnson2389@gmail.com>
2020-11-06 09:48:47 -0500

    initial commit of project skeleton

--- Destini-iOS13/AppDelegate.swift
+++ Destini-iOS13/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  Destini-iOS13
 //
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Alex Johnson on 11/06/2020.
+//  Copyright © 2020 Xander Apps. All rights reserved.
 //
 
 import UIKit
